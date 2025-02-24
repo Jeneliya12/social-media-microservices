059f64317f67f59a012b0ceafad2147678744153
Jeneliya Gurung <gurungjeneliya@gmail.com>
2025-02-24 00:02:12 -0500

    login user using apigateway

--- identity-service/src/controllers/identity-controller.js
+++ identity-service/src/controllers/identity-controller.js
@@ -1,20 +1,15 @@
 const User = require("../models/User");
 const logger = require("../utils/logger");
-const { validateRegistration } = require("../utils/validation");
-const generateTokens = require("../utils/generateTokens"); 
+const { validateRegistration, validateLogin } = require("../utils/validation");
+const generateTokens = require("../utils/generateTokens");
 
-// User registration
 const registerUser = async (req, res) => {
     logger.info("Registration endpoint hit...");
     try {
-        // Validate the schema
         const { error } = validateRegistration(req.body);
         if (error) {
-            logger.warn("Validation error", error.details[0].message);
-            return res.status(400).json({
-                success: false,
-                message: error.details[0].message,
-            });
+            logger.warn(`Validation error: ${error.details[0].message}`);
+            return res.status(400).json({ success: false, message: error.details[0].message });
         }
 
         const { email, password, username } = req.body;
@@ -22,15 +17,12 @@ const registerUser = async (req, res) => {
         let user = await User.findOne({ $or: [{ email }, { username }] });
         if (user) {
             logger.warn("User already exists");
-            return res.status(400).json({
-                success: false,
-                message: "User already exists",
-            });
+            return res.status(400).json({ success: false, message: "User already exists" });
         }
 
         user = new User({ username, email, password });
         await user.save();
-        logger.info("User saved successfully", user._id);
+        logger.info(`User saved successfully: ${user._id}`);
 
         const tokens = await generateTokens(user);
 
@@ -38,15 +30,49 @@ const registerUser = async (req, res) => {
             success: true,
             message: "User registered successfully!",
             accessToken: tokens.accessToken,
-            refreshToken: tokens.refreshToken
+            refreshToken: tokens.refreshToken,
         });
     } catch (e) {
         logger.error("Registration error occurred", e);
-        res.status(500).json({
-            success: false,
-            message: "Internal server error"
-        });
+        res.status(500).json({ success: false, message: "Internal server error" });
     }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+    logger.info("Login endpoint hit...");
+    try {
+        const { error } = validateLogin(req.body);
+        if (error) {
+            logger.warn(`Validation error: ${error.details[0].message}`);
+            return res.status(400).json({ success: false, message: error.details[0].message });
+        }
+
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            logger.warn("Invalid user");
+            return res.status(400).json({ success: false, message: "Invalid credentials" });
+        }
+
+        const isValidPassword = await user.comparePassword(password);
+        if (!isValidPassword) {
+            logger.warn("Invalid password");
+            return res.status(400).json({ success: false, message: "Invalid password" });
+        }
+
+        const { accessToken, refreshToken } = await generateTokens(user);
+
+        res.json({
+            success: true,
+            accessToken,
+            refreshToken,
+            userId: user._id,
+        });
+    } catch (e) {
+        logger.error("Login error occurred", e);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+};
+
+module.exports = { registerUser, loginUser };
--- identity-service/src/models/User.js
+++ identity-service/src/models/User.js
@@ -1,54 +1,55 @@
 const mongoose = require("mongoose");
 const argon2 = require("argon2");
 
-const userSchema = new mongoose.Schema({
-    username : {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
     },
-    email : {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true
-    },
-    password : {
-        type : String,
-        required : true
-    },
-    createdAt : {
-        type : Date,
-        default : Date.now
-    }
-},  {
-        timestamps: true
+    {
+        timestamps: true,
     }
 );
 
-userSchema.pre("save", async function(next) {
-    if(!this.isModified("password")){
-        try{
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        try {
             this.password = await argon2.hash(this.password);
-        }
-        catch(error){
+        } catch (error) {
             return next(error);
         }
     }
+    next();
 });
 
-userSchema.methods.comparePassword = async function(candidatePassword){
+userSchema.methods.comparePassword = async function (candidatePassword) {
     try {
-        return await argon2.verify(this.password, candidatePassword); 
-        }
-        catch(error) {
-        throw error
-    } 
-}
+        return await argon2.verify(this.password, candidatePassword);
+    } catch (error) {
+        return false;
+    }
+};
 
-userSchema.index({username : "text"});
+userSchema.index({ username: "text" });
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
